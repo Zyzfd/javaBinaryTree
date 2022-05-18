567100df5b1dd2ffff79e75e61c903552f4e124c
Zyzfd <vladkalihev8@gmail.com>
2022-05-18 20:00:24 +0400

    ok

--- zyzf.java
+++ zyzf.java
@@ -5,31 +5,43 @@ public class zyzf {
         final int kolvo_proverok = 50000000;
         
         for (int kolvo = 1000; kolvo < 100000; kolvo += 1000) {
-            Tree tree = new Tree();
-
-            long time_m;
-            //long time_t;
-
+            long memObjNow = Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
+            //Tree tree = new Tree();
             int[] massZn = new int[kolvo];
+            long memObj = Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
+
+            //long time_m;
+            //long time_t;
+            
             Random rand = new Random();
+            int value[] = new int[kolvo];
             for (int i = 0; i < kolvo; i++) {
-                int value = rand.nextInt(100);
-                //tree.insertNode(value);
-                massZn[i] = value;
+                value[i] = rand.nextInt(100);
             }
+
+            long memNow = Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
+            for (int i = 0; i < kolvo; i++) {
+                //tree.insertNode(value[i]);
+                massZn[i] = value[i];
+            }
+
+            for (int i = 0; i < kolvo_proverok; i++) {
+                Arrays.binarySearch(massZn, value[753]);
+            }
+            long mem = Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
 
             Arrays.sort(massZn);
 
-            int[] values = new int[kolvo_proverok];
-            for (int i = 0; i < kolvo_proverok; i++) {
-                values[i] = rand.nextInt(100);
-            }
+            // int[] values = new int[kolvo_proverok];
+            // for (int i = 0; i < kolvo_proverok; i++) {
+            //     values[i] = rand.nextInt(100);
+            // }
 
-            long timeNow = System.currentTimeMillis();
-            for (int i = 0; i < kolvo_proverok; i++) {
-                Arrays.binarySearch(massZn, values[i]);
-            }
-            time_m = System.currentTimeMillis() - timeNow;
+            // long timeNow = System.currentTimeMillis();
+            // for (int i = 0; i < kolvo_proverok; i++) {
+            //     Arrays.binarySearch(massZn, values[i]);
+            // }
+            // time_m = System.currentTimeMillis() - timeNow;
 
             // long timeNow = System.currentTimeMillis();
             // for (int i = 0; i < kolvo_proverok; i++) {
@@ -37,8 +49,9 @@ public class zyzf {
             // }
             // time_t = System.currentTimeMillis() - timeNow;
 
-            System.out.println(time_m);
+            //System.out.println(time_m);
             //System.out.println(time_t);
+            System.out.println(((mem-memNow)/1024) + ((memObj-memObjNow)/1024));
         }
     }
 }
